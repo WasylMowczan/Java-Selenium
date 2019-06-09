f6c6b4fbd594f6c7e8d93389ce54238b182f23e1
Wasyl Mowczan <movchanvasyl@gmail.com>
2019-06-09 15:24:59 +0200

    add test for checkbox

---
+++ src/test/java/CheckBoxesTests.java
@@ -0,0 +1,45 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+import static org.testng.Assert.assertFalse;
+
+
+public class CheckBoxesTests {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void beforeTest(){
+        System.setProperty("webdriver.chrome.driver", "C:/bin/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.navigate().to("http://theinternet.przyklady.javastart.pl/checkboxes");
+    }
+
+    @Test
+    public void checkBoxesTest(){
+        WebElement checkbox1 = driver.findElement(By.xpath("//*[@id=\"checkboxes\"]/input[1]"));
+        WebElement checkbox2 = driver.findElement(By.xpath("//*[@id=\"checkboxes\"]/input[2]"));
+
+        assertFalse(checkbox1.isSelected());
+        assertTrue(checkbox2.isSelected());
+
+        checkbox1.click();
+        checkbox2.click();
+
+        assertTrue(checkbox1.isSelected());
+        assertFalse(checkbox2.isSelected());
+    }
+
+    @AfterMethod
+    public void afterTest(){
+        driver.close();
+        driver.quit();
+    }
+
+
+}

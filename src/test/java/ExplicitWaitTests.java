7c82dc23fa950d93d5259d30987c6ad3b91a9093
Wasyl Mowczan <movchanvasyl@gmail.com>
2019-06-17 21:30:20 +0200

    add test for Explicit Wait

--- src/test/java/ExplicitWaitTests.java
+++ src/test/java/ExplicitWaitTests.java
@@ -2,7 +2,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
@@ -35,6 +34,14 @@ public class ExplicitWaitTests {
 
         WebElement messageAlert = driver.findElement(By.id("message"));
         assertEquals(messageAlert.getText(), "It's gone!");
+
+        WebElement addButton = driver.findElement(By.id("btn"));
+        addButton.click();
+
+        WaitUntil waitUntil = new WaitUntil(driver);
+        waitUntil.waitUntilElementVisible(checkbox);
+        assertTrue(checkbox.isDisplayed());
+        assertFalse(checkbox.isSelected());
     }
 
     @AfterMethod
